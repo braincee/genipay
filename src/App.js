33a04082103ab167b1ec48880bb6cf0b3ec3efd1
braincee <kwesi938@gmail.com>
2023-02-16 22:30:55 +0000

    Add all component and routing to the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LogIn from './components/Auth/LogIn';
+import SignUp from './components/Auth/SignUp';
+import Footer from './components/Pages/Footer';
+import HomePage from './components/Pages/HomePage';
+import SideBar from './components/Pages/SideBar';
+
 
 function App() {
   return (
     <div>
-      <p>GenioPay</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
